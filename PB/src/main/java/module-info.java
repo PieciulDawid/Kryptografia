6bd78bff9a3785c102a7f556992c5f1da1517d9b
Dawid Pieciul <pieciul.dawid@gmail.com>
2022-03-15 10:44:40 +0100

    Inicjalizacja 2

--- PB/src/main/java/module-info.java
+++ PB/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.example.zadanie1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
-    requires javafx.swing;
 
 
     opens com.example.zadanie1 to javafx.fxml;
