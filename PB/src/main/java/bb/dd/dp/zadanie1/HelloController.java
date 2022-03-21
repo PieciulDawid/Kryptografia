5d1227d82359cc47db76a075905b46e301b27712
Dawid Pieciul <pieciul.dawid@gmail.com>
2022-03-21 19:27:51 +0100

    Połączenie Backu z Frontem - wymagany debug bo back nie działa

--- PB/src/main/java/bb/dd/dp/zadanie1/HelloController.java
+++ PB/src/main/java/bb/dd/dp/zadanie1/HelloController.java
@@ -1,10 +1,15 @@
 package bb.dd.dp.zadanie1;
 
+import bb.dd.dp.impl.NumericCTEncryptor;
+import bb.dd.dp.impl.RailFenceEncryptor;
+import bb.dd.dp.impl.StringCTEncryptor;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.util.Arrays;
 
 
 // rf - Rail fence
@@ -67,5 +72,49 @@ public class HelloController {
     @FXML
     private Label rfOutput;
 
+    //Rail fence - szyfrowanie
+    @FXML
+    public void rfEncodeClick(){
+        RailFenceEncryptor railFenceEncryptor = new RailFenceEncryptor(Integer.parseInt(rfKey.getText()));
+        rfOutput.setText(railFenceEncryptor.encrypt(rfInput.getText()));
+    }
 
+    //Rail fence - deszyfrowanie
+    @FXML
+    public void rfDecodeClick(){
+        RailFenceEncryptor railFenceEncryptor = new RailFenceEncryptor(Integer.parseInt(rfKey.getText()));
+        rfOutput.setText(railFenceEncryptor.decrypt(rfInput.getText()));
+    }
+
+    //Przestawienie macierzowe - szyfrowanie
+    @FXML
+    public void pmEncodeClick(){
+        String pmKeyString = pmKey.getText();
+        int[] key = Arrays.stream(pmKeyString.split(",")).mapToInt(Integer::parseInt).toArray();
+        NumericCTEncryptor numericCTEncryptor = new NumericCTEncryptor(key,Integer.parseInt(pmDepth.getText()));
+        pmOutput.setText(numericCTEncryptor.encrypt(pmInput.getText()));
+    }
+
+    //Przestawienie macierzowe - deszyfrowanie
+    @FXML
+    public void pmDecodeClick(){
+        String pmKeyString = pmKey.getText();
+        int[] key = Arrays.stream(pmKeyString.split(",")).mapToInt(Integer::parseInt).toArray();
+        NumericCTEncryptor numericCTEncryptor = new NumericCTEncryptor(key,Integer.parseInt(pmDepth.getText()));
+        pmOutput.setText(numericCTEncryptor.decrypt(pmInput.getText()));
+    }
+
+    //Przestawienie macierzowe słowo - szyfrowanie
+    @FXML
+    public void pmsEncodeClick(){
+        StringCTEncryptor stringCTEncryptor = new StringCTEncryptor(pmsKey.getText(), Integer.parseInt(pmsDepth.getText()));
+        pmsOutput.setText(stringCTEncryptor.encrypt(pmsInput.getText()));
+    }
+
+    //Przestawienie macierzowe słowo - deszyfrowanie
+    @FXML
+    public void pmsDecodeClick(){
+        StringCTEncryptor stringCTEncryptor = new StringCTEncryptor(pmsKey.getText(), Integer.parseInt(pmsDepth.getText()));
+        pmsOutput.setText(stringCTEncryptor.decrypt(pmsInput.getText()));
+    }
 }
